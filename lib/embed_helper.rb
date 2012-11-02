5e244063735c1b0d3d21638c95c17f509655365d
David Díaz <diazglezdavid@gmail.com>
2012-11-02 20:37:52 +0000

    Added Wistia support
      * with custom sizes
      * fix spec from commit:162efd3

--- lib/embed_helper.rb
+++ lib/embed_helper.rb
@@ -28,6 +28,18 @@ module Embed
 				return link_to(url)
 			end
 		end
+                def wistia_embed(url, width, height)
+                        uri = URI('http://fast.wistia.com/oembed')
+                        params = { :format => 'json', :url => url, :width => width, :height => height }
+                        uri.query = params.collect { |k,v| "#{k}=#{CGI::escape(v.to_s)}" }.join('&')
+                        response = Net::HTTP.get(uri)
+                        if response
+                                html = JSON.parse(response)["html"]
+                                html.respond_to?(:html_safe) ? html.html_safe : html
+                        else
+                                return link_to(url)
+                        end
+                end
 		def embed(url, params = {})
                         params[:width] ||= 640
                         params[:height] ||= 390
@@ -37,6 +49,8 @@ module Embed
 				return vimeo_embed(url, params[:width], params[:height])
 			elsif url[/soundcloud.com/]
 				return soundcloud_embed(url)
+			elsif url[/wistia.com/]
+				return wistia_embed(url, params[:width], params[:height])
 			end
 		end
 	end
